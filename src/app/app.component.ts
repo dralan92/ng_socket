51a3f2846dfe1c908e1ebdefbde3c9c8966db50c
unknown <codename100.alan@gmail.com>
2018-10-27 02:42:49 -0400

    Installed socket.io

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as socketIo from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'NgSocket';
+
+  ngOnInit() : void{
+      
+    const socket = socketIo('http://localhost:3000');
+
+    socket.on('hello', (data)=>console.log(data));
+  }
 }
